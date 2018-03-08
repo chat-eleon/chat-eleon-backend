3656b5c43a67a2c31d93f574dd025f13e1ada0ff
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-03-08 17:29:24 +0700

    Create message schema

---
+++ models/message.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+  member: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  } ,
+  text: String,
+  grup: {
+    type: Schema.Types.ObjectId,
+    ref: 'Grup'
+  },
+  createdAt: {
+    type: Date,
+    default : Date.now
+  }
+});
+module.exports = mongoose.model('Message',messageSchema);
