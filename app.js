1cd298b4e1dcd0463acae510423da711117af2c2
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-03-08 17:53:13 +0700

    add api path to message routes

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/messages', messages);
+app.use('/api/messages', messages);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
